d06ada37f61047e7529a6a215559fed86ef4822b
Kevin <kingsager166@gmail.com>
2025-04-13 15:55:48 +1000

    fix(log): Optimize morgan records

--- app.js
+++ app.js
@@ -12,21 +12,20 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 app.set('trust proxy', true);
 morgan.token('remote-ip', req => req.ip);
-morgan.token('local-time', function () {
-    return new Date().toLocaleString();
-});
+morgan.token('local-time', () => new Date().toLocaleString());
 app.use(morgan(':local-time :remote-ip :method :url :status :res[content-length] - :response-time ms'));
-  
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'app', 'views'));
 
 app.use('/', Router);
+
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
